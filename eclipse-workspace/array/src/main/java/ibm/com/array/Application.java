06b71f66b3325d2ede3d693b8c0eeb9db65d6812
Thoshitha <thoshithagthoshi@gmail.com>
2025-04-29 22:20:35 +0530

    Static initializer priority first

---
+++ eclipse-workspace/array/src/main/java/ibm/com/array/Application.java
@@ -0,0 +1,15 @@
+package ibm.com.array;
+
+public class Application
+{
+	public static void main( String[] args )
+	{
+//		int nums[] = {2,3,4,5,6};/
+		int nums[];
+		nums = new int[5];
+		
+		for(int values:nums) {
+			System.out.println(values);
+		}
+	}
+}
