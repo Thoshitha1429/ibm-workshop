a2e6923e798a551d974f45d64fd49fb6744053fd
Thoshitha <thoshithagthoshi@gmail.com>
2025-04-30 10:16:54 +0530

    Static initializer priority first

--- eclipse-workspace/enums/src/main/java/ibm/com/enums/Application.java
+++ eclipse-workspace/enums/src/main/java/ibm/com/enums/Application.java
@@ -1,26 +1,31 @@
 package ibm.com.enums;
-enum Apple{
-	GoldenDel,
-	RedDel,
-	Gala
+
+enum Apple {
+    GoldenDel(10),
+    RedDel(15),
+    Gala(12);
+
+    private final int price;
+
+    Apple(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
 }
 
-public class Application
-{
-	public static void main( String[] args )
-	{
-//		Apple apple  = Apple.RedDel;
-//		System.out.println(apple);
-		
-//		apple  = Apple.Valueof("gala");
-//		System.out.println(apple);
-		
-		Apple[] apples = Apple.values();
-		
-		for (apple apple : apples) {
-			System.out.println(apple);
-		}
-		
-		
-	}
+public class Application {
+    public static void main(String[] args) {
+        Apple[] apples = Apple.values();
+
+        for (Apple apple : apples) {
+            System.out.println(apple + " costs " + apple.getPrice());
+        }
+
+        // Example of using valueOf to get an enum constant from a string
+        Apple selectedApple = Apple.valueOf("Gala");
+        System.out.println("Selected apple: " + selectedApple + " costs " + selectedApple.getPrice());
+    }
 }
